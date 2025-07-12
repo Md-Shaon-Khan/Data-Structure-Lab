0fb433c0b1f2f1f25e74a5385da68c89700131ce
Md-Shaon-Khan <shaon.iit52@gmail.com>
2025-07-12 10:49:23 +0600

    7th commit

--- Lab-09(Graph)/test.cpp
+++ Lab-09(Graph)/test.cpp
@@ -2,82 +2,72 @@
 using namespace std;
 
 const int MAX_VERTICES = 10;
-const int MAX_EDGES = 10;
+const int INF = 1e9;
 
-class Graph{
-    int adj[MAX_VERTICES][MAX_EDGES];
-    int count[MAX_VERTICES];
-    int vertices;
-    bool isDirected;
+class PrimsMST{
+   int adj[MAX_VERTICES][MAX_VERTICES];
+   int vertices,totalEdges;
 
-    public:
-    Graph(int v,bool directed){
-        vertices = v;
-        isDirected = directed;
+   public:
+   PrimsMST(int v){
+      vertices = v;
+      for(int i=0;i<vertices;i++){
+         for(int j=0;j<vertices;j++){
+            adj[i][j] = 0;
+         }
+      }
+   }
 
-        for(int i=0;i<vertices;i++){
-            count[i] = 0;
-            for(int j=0;j<MAX_EDGES;j++){
-                adj[i][j] = -1;
+   void addEdges(int e){
+      totalEdges = e;
+      for(int i=0;i<totalEdges;i++){
+         int u,v,w;
+         cin>>u>>v>>w;
+         adj[u][v] = w;
+         adj[v][u] = w;
+      }
+   }
+
+   int minIndex(int key[],bool visited[]){
+      int min = INF,minIndex = -1;
+
+      for(int i=0;i<vertices;i++){
+         if(!visited[i] && key[i]<min){
+            min = key[i];
+            minIndex = i;
+         }
+      }
+      return minIndex;
+   }
+
+   void buildMST(){
+      int parent[MAX_VERTICES];
+      bool visited[MAX_VERTICES];
+      int key[MAX_VERTICES];
+
+      for(int i=0;i<vertices;i++){
+         key[i] = INF;
+         visited[i] = false;
+      }
+
+      key[0] = 0;
+      parent[0] = -1;
+
+      for(int i=0;i<vertices-1;i++){
+         int u = minIndex(key,visited);
+         visited[u] = true;
+         for(int v=0;v<vertices;v++){
+            if(adj[u][v] && !visited[v] && adj[u][v]<key[v]){
+               key[v] = adj[u][v];
+               parent[v] = u;
             }
-        }
-    }
+         }
+      }
 
-    void addEdge(int u,int v){
-        adj[u][count[u]] = v;
-        count[u]++;
-
-        if(!isDirected){
-            adj[v][count[v]] = u;
-            count[v]++;
-        }
-    }
-
-    void BFS_Traversal(int start){
-        bool visited[MAX_VERTICES] = {false};
-        int queue[MAX_VERTICES];
-        int front = 0, rear = 0;
-        visited[start] = true;
-        queue[rear++] = start;
-
-        cout<<"BFS Traversal starting from vertex "<<start<<": ";
-
-        while(front<rear){
-            int current = queue[front++];
-            cout<<current<<" ";
-            for(int i=0;i<count[current];i++){
-                int neighbor = adj[current][i];
-                if(!visited[neighbor]){
-                    visited[neighbor] = true;
-                    queue[rear++] = neighbor;
-                }
-            }
-        }
-        cout<<endl;
-    }
+      int totalWeitgh = 0;
+      for(int i=1;i<vertices;i++){
+         cout<<parent[i]<<" "<<i<<" : "<<adj[i][parent[i]]<<endl;
+         totalWeitgh += adj[i][parent[i]];
+      }
+   }
 };
-
-int main() {
-    int vertices, edges, directed;
-    cout << "Enter number of vertices: ";
-    cin >> vertices;
-
-    cout << "Enter number of edges: ";
-    cin >> edges;
-
-    cout << "Is the graph directed? (1 = Yes, 0 = No): ";
-    cin >> directed;
-
-    Graph g(vertices, directed);
-
-    cout << "Enter edges (u v):" << endl;
-    for (int i = 0; i < edges; i++) {
-        int u, v;
-        cin >> u >> v;
-        g.addEdge(u, v);
-    }
-
-    g.BFS_Traversal(0);
-
-    return 0;
-}
